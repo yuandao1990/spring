5c9ded115a0ea7645c2fcd485cef20f07abe787f
Yuan Dao <dao.yuan.gl@gmail.com>
2023-09-05 19:49:12 +0800

    Update testFloat3.cpp

--- test/engine/System/testFloat3.cpp
+++ test/engine/System/testFloat3.cpp
@@ -1,4 +1,4 @@
-/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html test */
 
 #include <array>
 #include "System/float3.h"
